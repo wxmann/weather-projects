acfea16237bbbfb4d311463654a2156a9eca480d
wxmann <jim.z.tang@gmail.com>
2021-02-14 02:47:06 -0800

    Add SFO rain events clustering project

---
+++ proj_rain_events/events_calc.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import pairwise_distances
+from sklearn.cluster import DBSCAN
+
+
+def precip_events(df, eps, min_samples):
+    rain_hrs = df[df.precip_in >= 0.01]
+    
+    def to_hour(ns):
+        return ns / 3.6e12
+
+    dists = pairwise_distances(
+        rain_hrs.valid.values.reshape(-1, 1), 
+        metric=lambda x, y: to_hour(y - x),
+    )
+    db = DBSCAN(eps=eps, metric='precomputed', min_samples=min_samples)
+    clusts = db.fit_predict(dists, sample_weight=rain_hrs.precip_in.values / 0.01)
+    return clusts
